d1b57fb31999fadd3f70b8846605c703854a4d09
Jeffrey Covington <jcoving28@gmail.com>
2016-12-29 20:44:52 -0500

    Final update
    Player Class

--- character1.py
+++ character1.py
@@ -4,15 +4,16 @@ from grid import Command
 import os
 
 class Player:
-    ship_total = 17
+
 
     def __init__(self):
+        self.attack_list = list()
         # prompt for Names
         self.name = input('Name: ')
-        print("{}, Welcome to your ALLY WATERS! Here your will place your ships and record your opponents attacks.".format(self.name))
+        print("{}, Welcome to your ALLY WATERS! Here you will place your ships and record your opponents attacks.".format(self.name))
         self.ally = Ally()
-        pause = input("Continue?: press any key: ")
+        pause = input("Continue?: press Enter: ")
         print("{}, this is your command center. Here you will order your ships to attack ENEMY WATERS.\nUnfortunately, your opponent has jammed your radar and you must use logic and cunning to defeat your enemy".format(self.name))
         self.command = Command()
-        pause = input("Is your opponent ready? press any key: ")
+        pause = input("Is your opponent ready? press Enter: ")
         os.system('cls')
